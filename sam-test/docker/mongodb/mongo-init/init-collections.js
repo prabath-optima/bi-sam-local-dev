c96d36ce7698dadd0c4d3ce8e2f8c67ebd7c8802
prabath-optima <prabath.udakandage@optimatech.io>
2024-03-25 15:24:06 +1100

    Ensure the init-collection script checks for collections before creating

--- sam-test/docker/mongodb/mongo-init/init-collections.js
+++ sam-test/docker/mongodb/mongo-init/init-collections.js
@@ -1,22 +1,29 @@
 // init-collections.js
 
-// Create MfaRegex collection and insert an initial document
-db.createCollection("MfaRegex");
-db.MfaRegex.insert({
-  Pattern: "example-pattern",
-  Digit: 6,
-  Email: "example@example.com",
-  CreatedAt: new Date()
-});
+// Check if 'MfaRegex' collection exists
+if (!db.getCollectionNames().includes('MfaRegex')) {
+  // Create MfaRegex collection and insert an initial document
+  db.createCollection("MfaRegex");
+  db.MfaRegex.insert({
+    Pattern: "example-pattern",
+    Digit: 6,
+    Email: "example@example.com",
+    CreatedAt: new Date()
+  });
+}
 
-// Create MfaOtp collection and insert an initial document
-db.createCollection("MfaOtp");
-db.MfaOtp.insert({
-  Id: "unique-id-123",
-  Token: "token123",
-  ReceivedAt: new Date(),
-  ReceivedBy: "user@example.com",
-  SentBy: "service@example.com",
-  CreatedAt: new Date(),
-  ExpiredAt: new Date(new Date().getTime() + (30 * 60 * 1000)) // Expires in 30 minutes
-});
+// Check if 'MfaOtp' collection exists
+if (!db.getCollectionNames().includes('MfaOtp')) {
+  // Create MfaOtp collection and insert an initial document
+  db.createCollection("MfaOtp");
+  db.MfaOtp.insert({
+    Id: "unique-id-123",
+    Token: "token123",
+    ReceivedAt: new Date(),
+    ReceivedBy: "user@example.com",
+    SentBy: "service@example.com",
+    CreatedAt: new Date(),
+    ExpiredAt: new Date(new Date().getTime() + (30 * 60 * 1000)) // Expires in 30 minutes
+  });
+}
+
